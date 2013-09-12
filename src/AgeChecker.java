ff131b0abcd58d05ab310c118866a15edf8ef4c4
smellyyaaaa <student@192.168.0.35>
2013-09-12 10:38:21 -0400

    assignment 4

--- src/AgeChecker.java
+++ src/AgeChecker.java
@@ -1,15 +1,39 @@
+import javax.swing.JOptionPane;
+
 
 public class AgeChecker {
 
 	public static void main(String[] args) {
-		/*---------------------------------
-		 * In this file, you will recreate 
-		 * the age verification program we
-		 * created in class. When you're
-		 * finished, delete these instructions
-		 * before submitting.
-		 ---------------------------------*/
-
+		//prompts the user for information first and last name and year of birth
+		String firstName = JOptionPane.showInputDialog(null, "What is your First name?");	
+		String lastName = JOptionPane.showInputDialog(null, "What is your Last name?");
+		int yearOfBirth = Integer.parseInt(JOptionPane.showInputDialog(null, "What year were you born?"));	
+			
+			//call the functions verify age and concatenate names
+			boolean oldEnough = verifyLegalAge(yearOfBirth);
+			String fullName = concatenateNames(firstName, lastName);
+	
+			
+			//provides output corresponding to verify age
+			if(oldEnough == true) {
+				JOptionPane.showMessageDialog(null,  fullName + ". you may proceed with your shopping");
+			}
+			else {
+				JOptionPane.showMessageDialog(null,  fullName + ". you may not proceed with your shopping");
+			}
+			
+			
+			}
+			//gets the year of birth an int and determins if the person is 19 or older and returns a boolean vaklue
+		public static boolean verifyLegalAge(int yob) {
+			boolean legalAgeStatus = (2013-yob) >= 19;
+			return legalAgeStatus;
+		}
+		//gets the first and last name 2 strings and concatenates them and returns one string
+		public static String concatenateNames(String fn, String ln) {
+			String fullName = fn + " " + ln;
+			return fullName; 
+		}
 	}
 
-}
+
--- src/TestAgeChecker.java
+++ src/TestAgeChecker.java
@@ -1,18 +1,28 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 
 public class TestAgeChecker {
-	/*---------------------------------
-	 * In this file, you will write  
-	 * Unit Tests for the concatenateNames()
-	 * and verifyAgeRestriction() modules
-	 * you coded in the AgeChecker class.
-	 * 
-	 * When you're finished writing the tests,
-	 * run the debugger in Eclipse to verify
-	 * that the two modules function correctly.
-	 * 
-	 * Finally, delete these instructions, 
-	 * then commit and push the LiquorStore 
-	 * project to GitHub.
-	 ---------------------------------*/
 
+	AgeChecker tester = new AgeChecker();
+		
+			
+	@Test
+	public void testVerifyAgeRestrictionTrue() {
+		boolean result = tester.verifyLegalAge(1990);
+		assertTrue(result);
+	}
+	
+	@Test
+	public void testVerifyAgeRestrictionFalse() {
+		boolean result = tester.verifyLegalAge(1999);
+		assertFalse(result);
+	
+	}
+	
+	@Test
+	public void testConcatenateName() {
+		String result = tester.concatenateNames("Brandon", "Francis");
+		assertEquals(result, "Brandon Francis");
+	}
 }
