2ca527a7323f800105f5c65509c60b562fd781e5
clydewbrown <clydewbrown@gmail.com>
2013-09-12 07:54:06 -0400

    Source files for Assignment #4

---
+++ src/AgeChecker.java
@@ -0,0 +1,15 @@
+
+public class AgeChecker {
+
+	public static void main(String[] args) {
+		/*---------------------------------
+		 * In this file, you will recreate 
+		 * the age verification program we
+		 * created in class. When you're
+		 * finished, delete these instructions
+		 * before submitting.
+		 ---------------------------------*/
+
+	}
+
+}
---
+++ src/TestAgeChecker.java
@@ -0,0 +1,18 @@
+
+public class TestAgeChecker {
+	/*---------------------------------
+	 * In this file, you will write  
+	 * Unit Tests for the concatenateNames()
+	 * and verifyAgeRestriction() modules
+	 * you coded in the AgeChecker class.
+	 * 
+	 * When you're finished writing the tests,
+	 * run the debugger in Eclipse to verify
+	 * that the two modules function correctly.
+	 * 
+	 * Finally, delete these instructions, 
+	 * then commit and push the LiquorStore 
+	 * project to GitHub.
+	 ---------------------------------*/
+
+}
